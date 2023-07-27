3ad31b2e21dd90ac1ee9b3dc1a0a968d50eb3fd9
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-07-27 19:53:18 +1200

    feat: add basic card class

---
+++ FreeTheForest/Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+/* This class represents a card in the deck. */
+
+using System.Collections;
+
+public class Card
+{
+    public string name;
+
+    public Card(string name)
+    {
+        this.name = name;
+    }
+}
