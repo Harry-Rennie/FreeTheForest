9628c2e3180bead00b99a7388b65c70b00f85aea
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-07-28 10:47:02 +1200

    feat: implement discardPile and exiledPile, add multi card draw

--- FreeTheForest/Assets/Scripts/Deck.cs
+++ FreeTheForest/Assets/Scripts/Deck.cs
@@ -4,9 +4,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Deck : MonoBehaviour
 {
     public List<Card> deck;
+    public List<Card> discardPile;
+    public List<Card> exiledPile;
+
+    private Random random = UnityEngine.Random;
 
     public Deck()
     {
@@ -23,11 +28,48 @@ public class Deck : MonoBehaviour
         deck.Remove(card);
     }
 
-    public Card drawCard()
+    /// <summary>
+    /// Draws a single card from the deck and returns it.
+    /// </summary>
+    public Card DrawCard()
     {
         Card card = deck[0];
         deck.RemoveAt(0);
         return card;
+    }
+
+    public List<Card> DrawCards(int numCards)
+    {
+        List<Card> cards = new List<Card>();
+        // If there are not enough cards in the deck:
+        if (numCards > deck.Count)
+        {
+            // Add remaining cards in deck to cards to return
+            numCards -= deck.Count;
+            for (int i = 0; i < deck.Count; i++)
+            {
+                cards.Add(deck[0]);
+                deck.RemoveAt(0);
+            }
+            // Then shuffle discard pile into deck 
+            cards.AddRange(discardPile);
+            discardPile.Clear();
+            Shuffle();
+            // Then draw remaining cards
+            for (int i = 0; i < numCards; i++)
+            {
+                cards.Add(deck[0]);
+                deck.RemoveAt(0);
+            }
+        }
+
+        for (int i = 0; i < numCards; i++)
+        {
+            cards.Add(deck[0]);
+            deck.RemoveAt(0);
+        }
+        
+        return cards;
     }
 
     public void Shuffle()
@@ -35,7 +77,7 @@ public class Deck : MonoBehaviour
         for (int i = 0; i < deck.Count; i++)
         {
             Card temp = deck[i];
-            int randomIndex = Random.Range(i, deck.Count);
+            int randomIndex = random.Range(i, deck.Count);
             deck[i] = deck[randomIndex];
             deck[randomIndex] = temp;
         }
