4d80b136f3741a751951d09279b80bd608d25eef
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-07-27 19:52:49 +1200

    feat: add empty class for deck

---
+++ FreeTheForest/Assets/Scripts/Deck.cs
@@ -0,0 +1,10 @@
+/* This class contains the deck of cards and the methods to manipulate it. */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    
+}
