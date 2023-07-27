e9afada02b73efd4281c008164d9ecd58d141562
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-07-27 20:13:15 +1200

    feat: add some basic functions for manipulating the deck

--- FreeTheForest/Assets/Scripts/Deck.cs
+++ FreeTheForest/Assets/Scripts/Deck.cs
@@ -1,4 +1,4 @@
-/* This class contains the deck of cards and the methods to manipulate it. */
+/* This class contains the deck of deck and the methods to manipulate it. */
 
 using System.Collections;
 using System.Collections.Generic;
@@ -6,5 +6,38 @@ using UnityEngine;
 
 public class Deck : MonoBehaviour
 {
-    
+    public List<Card> deck;
+
+    public Deck()
+    {
+        deck = new List<Card>();
+    }
+
+    public void AddCard(Card card)
+    {
+        deck.Add(card);
+    }
+
+    public void RemoveCard(Card card)
+    {
+        deck.Remove(card);
+    }
+
+    public Card drawCard()
+    {
+        Card card = deck[0];
+        deck.RemoveAt(0);
+        return card;
+    }
+
+    public void Shuffle()
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            Card temp = deck[i];
+            int randomIndex = Random.Range(i, deck.Count);
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = temp;
+        }
+    }
 }
