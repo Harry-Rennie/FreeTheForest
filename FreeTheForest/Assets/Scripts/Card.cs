71ba23271980f125c6f5a656848d347a7088ecfd
bchorne <ben.chris.horne@gmail.com>
2023-07-30 17:16:47 +1200

    Added to Card template to make Asset creation available

--- FreeTheForest/Assets/Scripts/Card.cs
+++ FreeTheForest/Assets/Scripts/Card.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Card", menuName = "Card")]
 public class Card : ScriptableObject
 {
     public string title;
