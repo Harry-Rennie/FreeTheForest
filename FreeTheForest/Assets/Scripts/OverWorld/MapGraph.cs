8f7a02977494c5407ed64748c128d8f517b45331
Harry-Rennie <53240281+Harry-Rennie@users.noreply.github.com>
2023-08-07 04:32:09 +1200

    Done for now. Refactoring map generation. Nodes now group by depth first search, and separate into sub groups. Now need to work on connecting groups in a way that makes sense.

--- FreeTheForest/Assets/Scripts/OverWorld/MapGraph.cs
+++ FreeTheForest/Assets/Scripts/OverWorld/MapGraph.cs
@@ -1,29 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
-using UnityEditor.PackageManager;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MapGraph : MonoBehaviour
 {
+    //Todo: refactor more of this class into LineDrawer.cs
     private RectTransform mGraph;
     [SerializeField] RectTransform graphContainer;
     [SerializeField] private GameObject tilePrefab;
-    [SerializeField] int numberOfNodes = 10;
+    [SerializeField] int numberOfNodes;
+    [SerializeField] private LineDrawer lineDrawer;
     private float graphHeight;
     private float graphWidth;
-
     [SerializeField] int gridSizeX = 8;
     [SerializeField] int gridSizeY = 7;
-    [SerializeField] float cellWidth = 0.5f;
-    [SerializeField] float cellHeight = 0.5f;
-    //decide max encounters per level
-    // populate a list of encounters, with appropriate spacing between nodes, maybe keep track of the 'floor' number (Row)
-    // decide on which types of encounters we are going to have (boss, elite, normal mob, rest, reward etc.)
-    // assign a probability of each encounter type spawning to determine the node type to spawn.
-    // after the first row, we can have a random chance of spawning a node that is not a normal mob, but a rest, elite, boss, etc.
-    // the last and highest Y position node will always be the final boss of the stage.
 
     private void Awake()
     {
@@ -36,7 +28,10 @@ public class MapGraph : MonoBehaviour
     {
         List<Vector2> nodeLocations = GenerateRandomNodeLocations(numberOfNodes);
         nodeLocations = SortNodes(nodeLocations);
-        SpawnNodes(nodeLocations);
+        List<GameObject> nodes = SpawnNodes(nodeLocations);
+        ConnectNodes(nodes);
+        List<List<GameObject>> groups = GroupConnectedNodes(nodes);
+        ConnectGroups(groups);
     }
 
     private List<Vector2> GenerateRandomNodeLocations(int numberOfNodes)
@@ -53,7 +48,12 @@ public class MapGraph : MonoBehaviour
             return randomNodeLocations;
         }
 
-        // Generate three nodes in the second row
+        //top node always centered
+        Vector2 topNodePos = new Vector2(gridSizeX * 0.5f * cellWidth, (gridSizeY - 1) * cellHeight);
+        randomNodeLocations.Add(topNodePos);
+        numberOfNodes--;
+
+        //generate three nodes in the second row from the bottom
         for (int i = 0; i < 3; i++)
         {
             int gridX;
@@ -69,38 +69,26 @@ public class MapGraph : MonoBehaviour
             randomNodeLocations.Add(newNodePos);
         }
 
-        // generate nodes between the second row and top -1 (padding)
-        for (int i = 3; i < numberOfNodes - 1; i++)
+        // adjust to account for already added nodes
+        numberOfNodes -= 3;
+
+        //generate remaining nodes
+        for (int i = 0; i < numberOfNodes; i++)
         {
-            int gridX;
-            int gridY;
+            int gridX, gridY;
             Vector2 newNodePos;
 
             do
             {
                 gridX = Random.Range(1, gridSizeX - 1);
-                gridY = Random.Range(2, gridSizeY - 1);
+                gridY = Random.Range(2, gridSizeY - 1);  //excluding top and second row
 
-                // calculate the position of new node
                 newNodePos = new Vector2(gridX * cellWidth, gridY * cellHeight);
             }
             while (CheckNodeViolation(randomNodeLocations, newNodePos));
 
             randomNodeLocations.Add(newNodePos);
         }
-
-        // generate a node at the top (always boss)
-        int topGridX;
-        Vector2 topNodePos;
-
-        do
-        {
-            topGridX = Random.Range(1, gridSizeX - 1);
-            topNodePos = new Vector2(topGridX * cellWidth, (gridSizeY - 1) * cellHeight);
-        }
-        while (CheckNodeViolation(randomNodeLocations, topNodePos));
-
-        randomNodeLocations.Add(topNodePos);
 
         return randomNodeLocations;
     }
@@ -131,7 +119,7 @@ public class MapGraph : MonoBehaviour
         return tile;
     }
 
-    private void SpawnNodes(List<Vector2> nodeLocations)
+    private List<GameObject> SpawnNodes(List<Vector2> nodeLocations)
     {
         List<GameObject> nodes = new List<GameObject>();
         for (int i = 0; i < nodeLocations.Count; i++)
@@ -139,35 +127,164 @@ public class MapGraph : MonoBehaviour
             GameObject newNode = SpawnTile(nodeLocations[i]);
             nodes.Add(newNode);
         }
+        return nodes;
+    }
+    private void ConnectNodes(List<GameObject> nodes)
+    {
         for (int i = 0; i < nodes.Count; i++)
         {
-            GameObject nextHigherNode = FindNextHigherNode(nodes, i);
+            GameObject node = nodes[i];
+            Vector2 nodePosition = node.GetComponent<RectTransform>().anchoredPosition;
 
-            if (nextHigherNode != null)
+            //get nodes in row below the current node
+            List<GameObject> nodesInNextRow = GetNodesInRow(nodes, nodePosition.y);
+
+            //check if there are nodes in the row below
+            if (nodesInNextRow.Count > 0)
             {
-                DrawLine(nodes[i], nextHigherNode);
+                //find the closest node in the row below and draw a line
+                GameObject closestNode = GetClosestNode(nodePosition, nodesInNextRow);
+                DrawLineBetweenNodes(node, closestNode);
             }
         }
+        GroupConnectedNodes(nodes);
     }
 
-    private GameObject FindNextHigherNode(List<GameObject> nodes, int currentIndex)
+    private List<GameObject> GetNodesInRow(List<GameObject> nodes, float currentY)
     {
-        float currentY = nodes[currentIndex].GetComponent<RectTransform>().anchoredPosition.y;
-        GameObject nextHigherNode = null;
+        //dont do an exact comparison on two floats
+        float rowHeightTolerance = 0.1f;
+        //for each node get absolute value of node in row and return as list
+        return nodes.Where(n => Mathf.Abs(n.GetComponent<RectTransform>().anchoredPosition.y - currentY) > rowHeightTolerance).ToList();
+    }
 
-        for (int i = currentIndex + 1; i < nodes.Count; i++)
+    //sorting to find closest node
+    private GameObject GetClosestNode(Vector2 nodePosition, List<GameObject> nodes)
+    {
+        GameObject closestNode = null;
+        float closestDistance = float.MaxValue;
+
+        foreach (var node in nodes)
         {
-            float testY = nodes[i].GetComponent<RectTransform>().anchoredPosition.y;
-            if (testY > currentY)
+            Vector2 thisNodePosition = node.GetComponent<RectTransform>().anchoredPosition;
+            float distance = Vector2.Distance(nodePosition, thisNodePosition);
+
+            if (distance < closestDistance)
             {
-                currentY = testY;
-                nextHigherNode = nodes[i];
-                break;
+                closestDistance = distance;
+                closestNode = node;
             }
         }
 
-        return nextHigherNode;
+        return closestNode;
     }
+    private void DrawLineBetweenNodes(GameObject nodeA, GameObject nodeB)
+    {
+        lineDrawer.DrawLine(nodeA, nodeB);
+    }
+    //functions to perform depth first search on nodes with lines drawn between them, appending them to lists of groups of lists of nodes.
+    private List<List<GameObject>> GroupConnectedNodes(List<GameObject> nodes)
+    {
+        HashSet<GameObject> visitedNodes = new HashSet<GameObject>(); //this keeps track of the nodes that the depth first search finds.
+        List<List<GameObject>> groups = new List<List<GameObject>>();
+
+        foreach (var node in nodes)
+        {
+            if (!visitedNodes.Contains(node))
+            {
+                List<GameObject> group = new List<GameObject>();
+                DFS(node, group, nodes, visitedNodes);
+                groups.Add(group);
+            }
+        }
+
+        //logging information about the groups - remove when finished.
+        Debug.Log($"Total number of groups: {groups.Count}");
+        for (int i = 0; i < groups.Count; i++)
+        {
+            Debug.Log($"Group {i + 1} has {groups[i].Count} nodes.");
+        }
+
+        return groups;
+    }
+
+    private void DFS(GameObject currentNode, List<GameObject> group, List<GameObject> nodes, HashSet<GameObject> visitedNodes)
+    {
+        if (visitedNodes.Contains(currentNode))
+            return;
+
+        visitedNodes.Add(currentNode);
+        group.Add(currentNode);
+
+        //get neighboring nodes (directly connected nodes)
+        List<GameObject> neighbors = GetConnectedNeighbors(currentNode, nodes);
+
+        foreach (var neighbor in neighbors)
+        {
+            DFS(neighbor, group, nodes, visitedNodes);
+        }
+    }
+
+    private List<GameObject> GetConnectedNeighbors(GameObject node, List<GameObject> nodes)
+    {
+        List<GameObject> neighbors = new List<GameObject>();
+        Vector2 nodePosition = node.GetComponent<RectTransform>().anchoredPosition;
+
+        //checks nodes in the same row and nodes below for potential neighbours
+        List<GameObject> possibleNeighbors = GetNodesInRow(nodes, nodePosition.y);
+        possibleNeighbors.AddRange(GetNodesInRow(nodes, nodePosition.y - 1));  // -1 row
+
+        foreach (var neighbor in possibleNeighbors)
+        {
+            if (IsDirectlyConnected(node, neighbor))
+            {
+                neighbors.Add(neighbor);
+            }
+        }
+
+        return neighbors;
+    }
+
+    //using node locations to check if line renderer intersects with a node
+    private bool IsDirectlyConnected(GameObject node1, GameObject node2)
+    {
+        //get all LineRenderers in the scene
+        LineRenderer[] lineRenderers = GameObject.FindObjectsOfType<LineRenderer>();
+
+        foreach (LineRenderer lr in lineRenderers)
+        {
+            Vector3 startPoint = lr.GetPosition(0);
+            Vector3 endPoint = lr.GetPosition(1);
+
+            //check if either point of the line is within bounds of node1 or node2
+            if (IsPointInsideNodeBounds(startPoint, node1) && IsPointInsideNodeBounds(endPoint, node2) ||
+                IsPointInsideNodeBounds(startPoint, node2) && IsPointInsideNodeBounds(endPoint, node1))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsPointInsideNodeBounds(Vector3 point, GameObject node)
+    {
+        RectTransform rt = node.GetComponent<RectTransform>();
+
+        Vector3[] corners = new Vector3[4];
+        rt.GetWorldCorners(corners);
+
+        bool insideX = point.x >= corners[0].x && point.x <= corners[2].x;
+        bool insideY = point.y >= corners[0].y && point.y <= corners[2].y;
+
+        return insideX && insideY;
+    }
+
+    private void ConnectGroups(List<List<GameObject>> groups)
+    {
+        //todo
+    }
+
     public void RegenerateNodes()
     {
         // Clear the current graph first
@@ -180,57 +297,30 @@ public class MapGraph : MonoBehaviour
         }
         // Regenerate nodes and spawn a new graph
         List<Vector2> nodeLocations = GenerateRandomNodeLocations(numberOfNodes);
-        SpawnNodes(nodeLocations);
+        nodeLocations = SortNodes(nodeLocations);
+        List<GameObject> nodes = SpawnNodes(nodeLocations);
+        ConnectNodes(nodes);
+        List<List<GameObject>> groups = GroupConnectedNodes(nodes);
+        ConnectGroups(groups);
     }
 
+    //sorts from top to bottom
     private List<Vector2> SortNodes(List<Vector2> nodeLocations)
     {
         nodeLocations.Sort((node1, node2) =>
         {
             // compare y
-            int yComparison = node1.y.CompareTo(node2.y);
+            int yComparison = node2.y.CompareTo(node1.y);
             if (yComparison != 0)
             {
-                // if y is different, order by y (ascending)
                 return yComparison;
             }
             else
             {
-                // if y-coordinates is the same, order by x
                 return node1.x.CompareTo(node2.x);
             }
         });
 
         return nodeLocations;
-    }
-
-    private void DrawLine(GameObject tileA, GameObject tileB)
-    {
-        // get rect transforms of tiles to draw from
-        RectTransform rectA = tileA.GetComponent<RectTransform>();
-        RectTransform rectB = tileB.GetComponent<RectTransform>();
-
-        // add line renderer component
-        LineRenderer lr;
-        if (tileA.GetComponent<LineRenderer>() != null)
-        {
-            lr = tileA.GetComponent<LineRenderer>();
-        }
-        else
-        {
-            lr = tileA.AddComponent<LineRenderer>();
-        }
-
-        // set number of points in LineRenderer
-        lr.positionCount = 2;
-
-        // set positions with rect transforms position, to get world space pos
-        // A start, B end
-        lr.SetPosition(0, rectA.position);
-        lr.SetPosition(1, rectB.position);
-        lr.sortingOrder = 1;
-        lr.material = new Material(Shader.Find("Sprites/Default"));
-        lr.startColor = lr.endColor = Color.black;
-        lr.startWidth = lr.endWidth = 0.1f;
     }
 }
