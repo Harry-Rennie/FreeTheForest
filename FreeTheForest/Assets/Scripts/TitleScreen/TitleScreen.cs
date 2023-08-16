4647f3261bcc65140621ede68ef99106ca6c153f
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-08-16 21:41:19 +1200

    script: add TitleScreen script from gobbler's

--- FreeTheForest/Assets/Scripts/TitleScreen/TitleScreen.cs
+++ FreeTheForest/Assets/Scripts/TitleScreen/TitleScreen.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TitleScreen : MonoBehaviour
 {
+    [SerializeField] public GameObject audioManager;
+    [SerializeField] public GameObject volumeOn;
+    [SerializeField] public GameObject volumeOff;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(volumeOn.activeSelf == true)
+        {
+            audioManager.GetComponent<AudioManager>().PlayMusic("ForestTheme");
+        }
+        else
+        {
+            audioManager.GetComponent<AudioManager>().PlayMusic("ForestTheme");
+            audioManager.GetComponent<AudioManager>().ToggleMusic();
+        }
+
+        //SceneManager.LoadSceneAsync("MainMenu", LoadSceneMode.Additive);      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
