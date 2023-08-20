fa58d222c263868f910da3baf0dea8cade32c8fa
bchorne <ben.chris.horne@gmail.com>
2023-08-20 13:08:16 +1200

    Created scriptable object for an enemy, added to Asset creation.

---
+++ FreeTheForest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+//Holds enemy behaviour and stats for loading into Entity objects
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy")]
+public class Enemy : ScriptableObject
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
