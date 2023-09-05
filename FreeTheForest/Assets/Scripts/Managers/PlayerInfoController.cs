e3d8c0a13632e601be571c676e4fd6baae22ec2a
JohnathanGlasgow <glasgjg1@student.op.ac.nz>
2023-09-05 18:57:54 +1200

    script: add player stats to PlayerInfoController

--- FreeTheForest/Assets/Scripts/Managers/PlayerInfoController.cs
+++ FreeTheForest/Assets/Scripts/Managers/PlayerInfoController.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+/// <summary>
+/// This class is used to store the player's information. It is used to pass information between scenes.
+/// </summary>
 public class PlayerInfoController : MonoBehaviour
 {
     public TMP_Text m_TextMeshPro;
     public static PlayerInfoController instance;
+    public int MaxHealth;
+    public int CurrentHealth;
+    public int Strength;
+    public int Defense;
     public List<Card> playerDeck = new List<Card>(); //Holds the players current deck as a list of cards.
 
     public List<Enemy> currentEnemies = new List<Enemy>(); //Battle reads from here to load in enemies to battle scene.
