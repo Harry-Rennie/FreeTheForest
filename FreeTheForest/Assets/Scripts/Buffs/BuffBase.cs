b5905692ebc0df573941922f65cf729774a34391
bchorne <ben.chris.horne@gmail.com>
2023-09-19 21:50:54 +1200

    Added Buffname property and Tick method to base class

--- FreeTheForest/Assets/Scripts/Buffs/BuffBase.cs
+++ FreeTheForest/Assets/Scripts/Buffs/BuffBase.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public abstract class BuffBase
 {
     public int stacks;
-    public bool canStack;
-    public bool isPermanent;
-    public Entity target;
+    public string buffName; //Name for buff. Used for comparing when re-applying buffs
+    public bool canStack; //Can we have more than 1 stack of this buff?
+    public bool isPermanent; //Should we reduce stacks by 1 each round for this buff? Set to false for custom stack drain behaviour.
+    public Entity target; //What entity does this buff act on?
+    public bool eachTurn; //Does this buff do something each turn?
 
     public void Activate()
     {
@@ -26,4 +28,6 @@ public abstract class BuffBase
     public abstract void Apply();
 
     public abstract void End();
+
+    public abstract void Tick();
 }
