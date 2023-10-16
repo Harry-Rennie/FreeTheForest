6ef41cb295933979d9335b5a4d23db4b23739022
JohnathanGlasgow <johnathanglasgow@gmail.com>
2023-10-16 22:31:20 +1300

    feat: display discard/exile piles

--- FreeTheForest/Assets/Scripts/DiscardDisplay.cs
+++ FreeTheForest/Assets/Scripts/DiscardDisplay.cs
@@ -9,9 +9,9 @@ using UnityEngine.UI;
 public class DiscardDisplay : MonoBehaviour
 {
     /// <summary>
-    /// The UI element that contains the scrollRect.
+    /// The UI element that contains the ScrollRect.
     /// </summary>
-    [SerializeField] private GameObject scrollRect;
+    [SerializeField] private ScrollRect ScrollRect;
     /// <summary>
     /// The UI element that contains the scrollable card content.
     /// </summary>
@@ -20,6 +20,7 @@ public class DiscardDisplay : MonoBehaviour
     /// The prefab configured to display a card's info in the compendium.
     /// </summary>
     [SerializeField] private GameObject compendiumCardPrefab;
+    [SerializeField] private GameObject exiledShroud;
     private Deck deck;
     private BattleManager battleManager;
 
@@ -44,7 +45,7 @@ public class DiscardDisplay : MonoBehaviour
     {
         battleManager = FindObjectOfType<BattleManager>();
         deck = battleManager.deck;
-        testDiscard(50, 10);
+        testDiscard(20, 10);
         // resize the height of the compendium if necessary
         setCompendiumHeight();
         // populate the compendium with the player's cards
@@ -56,7 +57,7 @@ public class DiscardDisplay : MonoBehaviour
     /// </summary>
     private void displayCards()
     {
-        // for each card in playerCards), add a CompendiumCard prefab to the cardContainer
+        // for each card in deck.DiscardPile, add a CompendiumCard prefab to the cardContainer
         // then display the card info with the CompendiumCard.CompendiumCardDisplay function
         foreach (Card card in deck.DiscardPile)
         {
@@ -70,6 +71,39 @@ public class DiscardDisplay : MonoBehaviour
 
             // display the card info
             compendiumCard.GetComponent<CompendiumCard>().DisplayCard(card);
+        }
+        // for each card in deck.ExiledPile, add a CompendiumCard prefab to the cardContainer
+        // then display the card info with the CompendiumCard.CompendiumCardDisplay function
+        foreach (Card card in deck.ExiledPile)
+        {
+            GameObject compendiumCard = Instantiate(compendiumCardPrefab);
+            // set the parent of the compendiumCard to the cardContainer
+            compendiumCard.transform.SetParent(cardContainer.transform);
+            // again, for reasons unknown, the scale and z position of the card is changed to weird values when the parent is set
+            // these lines of code resets the scale and z position to their original values
+            compendiumCard.transform.localScale = new Vector3(1, 1, 1);
+            compendiumCard.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+
+            // Find the "CardImg" child object
+            Transform cardImg = compendiumCard.transform.Find("CardImg");
+
+            if (cardImg != null)
+            {
+                // Find the "ExiledShroud" child object within "CardImg" and activate it
+                Transform exiledShroud = cardImg.transform.Find("Exiled Shroud");
+
+                if (exiledShroud != null)
+                {
+                    exiledShroud.gameObject.SetActive(true);
+                }
+            }
+
+            // Display the card info using the CompendiumCard component
+            CompendiumCard compendiumCardComponent = compendiumCard.GetComponent<CompendiumCard>();
+            if (compendiumCardComponent != null)
+            {
+                compendiumCardComponent.DisplayCard(card);
+            }
         }
 
     }
@@ -95,17 +129,17 @@ public class DiscardDisplay : MonoBehaviour
         // calculate the amount of cards that can fit in a row
         int amtCardsPerRow = compendiumWidth / cardCellWidth;
         // calculate the amount of rows needed to display all the cards
-        int rows = (int)Mathf.Ceil((float)deck.DiscardPile.Count / amtCardsPerRow);
+        int rows = (int)Mathf.Ceil((float)deck.DiscardPile.Count + deck.ExiledPile.Count / amtCardsPerRow);
 
         // set the height of the cardContainer to the height of the cards * the amount of rows, or the minimum height of the compendium
         cardContainerRect.sizeDelta = (rows > minCompendiumHeight / cardCellHeight) ?
             new Vector2(cardContainerRect.sizeDelta.x, cardCellHeight * rows) : new Vector2(cardContainerRect.sizeDelta.x, minCompendiumHeight);
 
         // scroll to the top of the compendium
-        scrollRect.GetComponent<ScrollRect>().verticalNormalizedPosition = 1;
+        ScrollRect.verticalNormalizedPosition = 1;
     }
 
-        private void testDiscard(int discardAmt = 5, int exileAmt = 5)
+    private void testDiscard(int discardAmt = 5, int exileAmt = 5)
     {
         Card testCard = new Card();
 
