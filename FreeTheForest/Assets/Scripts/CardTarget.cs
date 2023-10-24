1da393be44121eed734f84e9801de743314b6d5d
DESKTOP-410A3IA\Harry <harry.rennie@hotmail.co.nz>
2023-10-24 17:53:44 +1300

    feat: basic card targeting indicators. fix: scene fade animation exit. todo: further hand logic & ux logic.

--- FreeTheForest/Assets/Scripts/CardTarget.cs
+++ FreeTheForest/Assets/Scripts/CardTarget.cs
@@ -2,42 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CardTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     BattleManager battle;
     Entity enemy; // Self
+    GameObject valid;
+    GameObject invalid;
 
     private void Awake()
     {
         battle = FindObjectOfType<BattleManager>();
         enemy = GetComponent<Entity>();
+        valid = transform.GetChild(1).gameObject;
+        invalid = transform.GetChild(2).gameObject;
     }
 
     // Implement the OnPointerEnter method from IPointerEnterHandler
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (enemy == null || !enemy.isPlayer) // Sanity check
-        {
-            Debug.Log("Fighter Null");
-            battle = FindObjectOfType<BattleManager>();
-            enemy = GetComponent<Entity>();
-        }
         if(battle.selectedCard != null && battle.selectedCard.card.cardType != Card.CardType.Attack) //then target self
         {
             if(enemy.isPlayer)
             {
                 battle.cardTarget = enemy;
                 Debug.Log("Targeting " + enemy);
+                valid.SetActive(true);
+            }
+        }
+        if(battle.selectedCard != null && battle.selectedCard.card.cardType == Card.CardType.Attack) //then target enemy
+        {
+            if(enemy.isPlayer)
+            {
+                invalid.SetActive(true);
             }
         }
         // Additional logic when the pointer enters the object area
         if (battle.selectedCard != null && battle.selectedCard.card.cardType == Card.CardType.Attack) // If Card Selected and The Card Is An Attack...
         {
+            if(enemy.isPlayer)
+            {
+                invalid.SetActive(true);
+            }
             if(!enemy.isPlayer)
             {
                 battle.cardTarget = enemy;
                 Debug.Log("Targeting " + enemy);
+                valid.SetActive(true);
             }
         }
     }
@@ -45,6 +57,8 @@ public class CardTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     // Implement the OnPointerClick method from IPointerClickHandler
     public void OnPointerClick(PointerEventData eventData)
     {
+        valid.SetActive(false);
+        invalid.SetActive(false);
         //check to see if battle is over and is a reward card
         if(battle.battleOver == true && battle.RewardPanel.activeSelf == true)
         {
@@ -72,6 +86,8 @@ public class CardTarget : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
     {
         // Logic to execute when the pointer exits the object area.
         battle.cardTarget = null;
+        valid.SetActive(false);
+        invalid.SetActive(false);
     }
 
     public void AddRewardCard(PointerEventData eventData)
